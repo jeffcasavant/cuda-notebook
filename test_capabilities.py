3d491faf4bd209e4064d289155cc5a3e8877b01e
Jeff Casavant <jeff.casavant@gmail.com>
2022-12-02 18:59:17 -0500

    Print used device in cap test

--- test_capabilities.py
+++ test_capabilities.py
@@ -1,18 +1,4 @@
-from __future__ import print_function
+import tensorflow as tf
 
-import torch
-
-x = torch.rand(5, 3)
-print(x)
-
-if not torch.cuda.is_available():
-    print("Cuda is available")
-    device_id = torch.cuda.current_device()
-    gpu_properties = torch.cuda.get_device_properties(device_id)
-    print(
-        "Found {torch.cuda.device_count()} GPUs available. Using GPU {device_id} "
-        "({gpu_properties.name}) of compute capability {gpu_properties.major}.{gpu_properties.minor} "
-        "with {gpu_properties.total_memory / 1e9} total memory."
-    )
-else:
-    print("No cuda")
+a = tf.constant([1, 2, 3])
+print(a.device)
